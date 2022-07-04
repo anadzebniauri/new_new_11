f7cea124147fb6593f6e908084f51e772eec617e
anadzebniauri <ana.dzebniauri@space.ge>
2022-07-04 21:05:44 +0400

    Initial Commit

---
+++ new_new_11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new_new_11
+//
+//  Created by Ana Dzebniauri on 04.07.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
